3ec8cd5aa7ffe7244e8e329b35bb0432a2bee3a0
Flex Coder <jaychonkar617@gmail.com>
2025-05-05 09:12:09 +0530

    view.js

--- api/view.js
+++ api/view.js
@@ -3,14 +3,13 @@ const path = require('path');
 
 module.exports = async (req, res) => {
   const filePath = path.join('/tmp', 'ips.json');
-  
+
   if (!fs.existsSync(filePath)) {
     res.status(404).json({ message: "No saved IP data found." });
     return;
   }
 
   const savedIps = JSON.parse(fs.readFileSync(filePath));
-
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(savedIps, null, 2));
 };
